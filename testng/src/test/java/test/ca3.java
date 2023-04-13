5e3abfb95d91a8a4f51d8d8ed1a8349c17c69800
Mohi <mohiuddin3551@gmail.com>
2023-04-13 13:23:39 +0530

    done

--- testng/src/test/java/test/ca3.java
+++ testng/src/test/java/test/ca3.java
@@ -1,6 +1,10 @@
 package test;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,12 +13,12 @@ import org.testng.annotations.*;
 
 public class ca3 {
 
-    private WebDriver driver;
+     WebDriver driver;
 
     @BeforeTest
     @Parameters({"browser", "url"})
     public void setup(String browser, String url) {
-        
+
         if (browser.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
             driver = new ChromeDriver();
@@ -27,10 +31,19 @@ public class ca3 {
     }
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws IOException {
+        FileInputStream file = new FileInputStream("resources/config.properties");
+        Properties prop = new Properties();
+        prop.load(file);
+        String username = prop.getProperty("username");
+        String password = prop.getProperty("password");
        
-        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("mohiuddin3551@gmail.com");
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("sdjhgudydvk");
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys(username);
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[2]/div/div[1]/div/div/button/span")).click();
+
+
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
 
         
         driver.findElement(By.xpath("//button[@type='submit']")).click();
